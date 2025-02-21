2ff15e772c4bb8c0db113ffe371d16e2e98f5720
Steve Cliff <steve@devcloud.guru>
2025-02-21 12:25:02 +0000

    Update next.config.js

--- site/next.config.js
+++ site/next.config.js
@@ -2,10 +2,10 @@
 
 const nextConfig = {
   env: {
-    name: 'Kasm Technologies',
-    description: 'The official store for Kasm supported workspaces.',
+    name: 'DevCloudGuru Kasm Registry',
+    description: 'A public repo for my specific Kasm use cases',
     icon: '/img/logo.svg',
-    listUrl: 'https://registry.kasmweb.com/',
+    listUrl: 'https://github.com/dcgsteve/kasm-registry/',
     contactUrl: 'https://kasmweb.com/support',
   },
   reactStrictMode: true,
